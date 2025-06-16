cf7cfc2b90e481d17d6a1b13f41ea64be4346f71
nithinv16 <72072710+nithinv16@users.noreply.github.com>
2025-06-16 15:27:48 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-
 require('dotenv').config();
-
 const express = require('express');
 const https = require('https');
 
@@ -10,14 +8,19 @@ const PORT = process.env.PORT || 3000;
 app.get('/send-otp', async (req, res) => {
   const { mobile, otp, secret } = req.query;
 
+  console.log("Received secret:", secret);
+  console.log("Expected secret:", process.env.HOOK_SECRET);
+  
   if (secret !== process.env.HOOK_SECRET) {
     return res.status(403).json({ error: 'Unauthorized' });
   }
 
-  const message = `Hello, your OTP is ${otp}`;
+  const company = 'dukaaOn-Rider'; // Change this to your actual app name
+  const message = `Use ${otp} as your OTP to access your ${company}, OTP is confidential and valid for 5 mins. This SMS sent by authkey.io`;
+
   const authKey = process.env.AUTHKEY;
-  const senderId = 'AUTHKY'; // or your sender ID
-  const countryCode = '91';  // modify if needed
+  const senderId = 'AUTHKY';
+  const countryCode = '91';
 
   const url = `https://console.authkey.io/request?authkey=${authKey}&mobile=${mobile}&country_code=${countryCode}&sms=${encodeURIComponent(message)}&sender=${senderId}`;
 
